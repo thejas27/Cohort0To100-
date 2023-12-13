f7a5ba844319fdc28fe1d35c631410a72748edeb
thejas27 <153344853+thejas27@users.noreply.github.com>
2023-12-14 02:45:44 +0530

    Add files via upload

---
+++ 01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -0,0 +1,15 @@
+/*
+    Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
+*/
+
+function wait(n) {
+    const p = new Promise(function(resolve){
+        console.log("inside promise")
+        setTimeout(function(){
+            resolve();
+        },n*1000);
+    })
+    return p;
+}
+
+module.exports = wait;
